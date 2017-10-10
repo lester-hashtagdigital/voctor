a75fc3df42eb79442c9a539b866bf54acd49f215
Lester Hurtado <lester@hashtagdigital.com>
2017-10-10 13:43:36 +0800

    Will be here

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Voctor was here!
+                    Voctor will be here!
                 </div>
 
                 <div class="links">
